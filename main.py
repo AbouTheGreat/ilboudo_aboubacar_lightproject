c26bf778e02ea6fe449fa5cc007c37756b06d9e5
AbouTheGreat <72754592+AbouTheGreat@users.noreply.github.com>
2020-12-31 13:13:00 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 while True:
-    # if light level is greater than 6, then set neopixels off
+    # if light level is greater than 9, then set neopixels off
     print("light Level: " + str(input.light_level()))
     if input.light_level() >= 9:
         light.clear()
+
+    # if light level is greater than 6, then dim neopixels red
     elif input.light_level() <= 6:
-        # if light level is greater than 2, then dim neopixels red
         light.set_brightness(15)
         light.set_all(color.rgb(255, 0, 0))
     else:
--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 while (true) {
-    //  if light level is greater than 6, then set neopixels off
+    //  if light level is greater than 9, then set neopixels off
     console.log("light Level: " + ("" + input.lightLevel()))
     if (input.lightLevel() >= 9) {
         light.clear()
     } else if (input.lightLevel() <= 6) {
-        //  if light level is greater than 2, then dim neopixels red
+        //  if light level is greater than 6, then dim neopixels red
         light.setBrightness(15)
         light.setAll(color.rgb(255, 0, 0))
     } else {
